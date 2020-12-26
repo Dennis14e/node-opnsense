91dc3e1e750001e58e7dba74e7bb703206f61acb
Dennis Neufeld <info@dennis14e.de>
2020-12-26 16:13:54 +0100

    New opt verify_tls, new func wol_set, added linter

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 'use strict';
 
 const axios = require('axios');
+const https = require('https');
+
 require('axios-debug-log');
 
 class OPNSense {
-    constructor(baseURL, key, secret) {
+    constructor (baseURL, key, secret, verify_tls = true) {
+        baseURL = baseURL.replace(/\/?$/, '/');
         this._baseURL = baseURL + 'api';
         this._key = key;
         this._secret = secret;
+        this._verify_tls = Boolean(verify_tls);
+        this._debug = false;
 
         this.client = axios.create({
             baseURL: this._baseURL,
@@ -15,37 +20,47 @@ class OPNSense {
                 username: this._key,
                 password: this._secret,
             },
+            httpsAgent: new https.Agent({
+                rejectUnauthorized: this._verify_tls,
+            }),
         });
 
-        if (true === true) {
-            this.client.interceptors.request.use(function (config) {
+        if (this._debug === true) {
+            this.client.interceptors.request.use(config => {
                 console.log(config);
                 return config;
             });
         }
     }
 
-    async request(url, method = 'get', data = {}) {
+    async request (url, method = 'get', data = null) {
         const response = this.client({
             url: url,
             method: method,
             data: data,
         })
-        .then(function (data) {
-            console.log('Success: ' + JSON.stringify(data));
+        .then(response => {
+            return {
+                status: 'success',
+                result: response.data,
+            };
         })
-        .catch(function (error) {
+        .catch(error => {
             return {
                 status: 'error',
-                data: error.message,
+                result: error.message,
             };
         });
 
         return response;
     }
 
-    async wol_searchHost() {
-        return await this.request('/wol/wol/searchHost/');
+    async wol_searchHost () {
+        return await this.request('/wol/wol/searchHost');
+    }
+
+    async wol_set (uuid) {
+        return await this.request('/wol/wol/set', 'post', { uuid: uuid });
     }
 }
 
