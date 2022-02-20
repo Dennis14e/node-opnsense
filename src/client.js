5d37f9c16161c413fb1037c621440ea6903f7239
Dennis Neufeld <git@dneufeld.net>
2022-02-20 22:40:40 +0100

    Drop axios, use http/https modules

--- src/client.js
+++ src/client.js
@@ -6,9 +6,8 @@
  * @module OPNsense/Client
  */
 
-const axios = require('axios');
+const http = require('http');
 const https = require('https');
-const querystring = require('querystring');
 
 /**
  * OPNsense Client
@@ -23,22 +22,42 @@ class Client {
      * @param {boolean} verify_tls Check TLS certificates
      */
     constructor (baseURL, key, secret, verify_tls = true) {
-        baseURL = baseURL.replace(/\/?$/, '/');
-        this._baseURL = baseURL + 'api';
+        this._baseURL = new URL(baseURL.replace(/\/?$/, '/') + 'api');
         this._key = key;
         this._secret = secret;
         this._verify_tls = Boolean(verify_tls);
+        this._tls = this._baseURL.protocol === 'https:';
 
-        this.client = axios.create({
-            baseURL: this._baseURL,
-            auth: {
-                username: this._key,
-                password: this._secret,
+        this.options = {
+            hostname: this._baseURL.hostname,
+            port: (
+                this._baseURL.port !== ''
+                    ? parseInt(this._baseURL.port)
+                    : (this._tls ? 443 : 80)
+            ),
+            path: this._baseURL.pathname, // default
+            method: 'GET', // default
+            headers: {
+                'Authorization': 'Basic ' + Buffer.from(this._key + ':' + this._secret).toString('base64'),
+                'Accept':        'application/json',
+                'User-Agent':    'Node OPNsense API-Client',
             },
-            httpsAgent: new https.Agent({
+        };
+
+        let agent_options = {};
+
+        if (this._tls) {
+            this.httpModule = https;
+
+            agent_options = {
                 rejectUnauthorized: this._verify_tls,
-            }),
-        });
+            };
+        }
+        else {
+            this.httpModule = http;
+        }
+
+        this.options.agent = new this.httpModule.Agent(agent_options);
     }
 
     /**
@@ -51,8 +70,51 @@ class Client {
     response (status, result) {
         return {
             status: status,
-            result: result
+            result: result,
         };
+    }
+
+    /**
+     * Request function
+     *
+     * @param   {object}          options  HTTP options
+     * @param   {string}          postData URL-encoded POST data
+     * @returns {Promise<object>}          Response object
+     */
+    request (options, postData = '') {
+        return new Promise((resolve, reject) => {
+            const req = this.httpModule.request(options, (res) => {
+                if (res.statusCode < 200 || res.statusCode >= 300) {
+                    return reject('Status code ' + res.statusCode);
+                }
+
+                let buffer = [];
+                res.on('data', (chunk) => {
+                    buffer.push(chunk);
+                });
+
+                res.on('end', () => {
+                    try {
+                        buffer = JSON.parse(Buffer.concat(buffer).toString());
+                    }
+                    catch (e) {
+                        reject(e.message);
+                    }
+
+                    resolve(buffer);
+                });
+            });
+
+            req.on('error', (e) => {
+                reject(e.message);
+            });
+
+            if (postData !== '') {
+                req.write(postData);
+            }
+
+            req.end();
+        });
     }
 
     /**
@@ -63,19 +125,20 @@ class Client {
      * @returns {Promise}      Request promise
      */
     async get (url, data = {}) {
-        const response = this.client({
-            url: url,
-            method: 'get',
-            params: data,
+        data = new URLSearchParams(data).toString();
+
+        let options = this.options; // copy base options
+        options.path += url + '?' + data;
+
+        const req = this.request(options)
+        .then((res) => {
+            return this.response('success', res);
         })
-        .then(res => {
-            return this.response('success', res.data);
-        })
-        .catch(res => {
-            return this.response('error', res.message);
+        .catch((res) => {
+            return this.response('error', res);
         });
 
-        return response;
+        return req;
     }
 
     /**
@@ -86,22 +149,23 @@ class Client {
      * @returns {Promise}      Request promise
      */
     async post (url, data = {}) {
-        const response = this.client({
-            url: url,
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            data: querystring.stringify(data),
+        data = new URLSearchParams(data).toString();
+
+        let options = this.options; // copy base options
+        options.path += url;
+        options.method = 'POST';
+        options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+        options.headers['Content-Length'] = data.length;
+
+        const req = this.request(options, data)
+        .then((res) => {
+            return this.response('success', res);
         })
-        .then(res => {
-            return this.response('success', res.data);
-        })
-        .catch(res => {
-            return this.response('error', res.message);
+        .catch((res) => {
+            return this.response('error', res);
         });
 
-        return response;
+        return req;
     }
 }
 
